23c8b8e6bd6afdca8580fb0c59a3a54395c3eb16
Aaron <Harvey.AaronJ@Gmail.com>
2023-01-13 11:27:57 -0500

    Adds error usage to housingMap.js Maps over data fetched to create an object that becomes mapMarkers

--- force-app/main/default/lwc/housingMap/housingMap.js
+++ force-app/main/default/lwc/housingMap/housingMap.js
@@ -6,9 +6,23 @@ export default class HousingMap extends LightningElement {
   error;
 
   @wire(getHouses)
-  wiredHouses({ data }) {
+  wiredHouses({ error, data }) {
     if (data) {
-      console.log(data);
+      this.mapMarkers = data.map((element) => {
+        return {
+          location: {
+            Street: element.Address__c,
+            City: element.City__c,
+            State: element.State__c
+          },
+
+          title: element.Name
+        };
+      });
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.mapMarkers = undefined;
     }
   }
 }
